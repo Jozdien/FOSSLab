f69cec615b56561306babb99247296290918effe
Jozdien <joseverres42@gmail.com>
2020-01-30 23:30:17 +0530

    Final Changes for this set's part.

--- s2c1p5.sh
+++ s2c1p5.sh
@@ -1,30 +1,18 @@
-memlimit=10.0;
-cpulimit=10.0;
-while( true )
-do
-echo "Script running..."
-ps -e -o pmem=,cpu=,pid=,user=,comm= $|$ sort=-pmem $|$
-while read size cpu pid user comm
-do
-kill_mem=0
-kill_cpu=0
-if [ "\$USER" = "jose" ]
-then
-echo "Script running..."
-kill_mem =` echo "$size$>$$memlimit" $|$ bc `
-kill_cpu =` echo "$cpu$>$$cpulimit" $|$ bc `
-if [ "$kill_mem = 1" ]
-then
-echo "Process with PID $pid killed."
-kill $pid
-elif [ "$kill_cpu = 1" ]
-then
-echo "Process with PID $pid killed."
-kill $pid
-else
-continue
+#!/bin/sh
+
+if [ ! $# -eq 1 ]; then
+	echo Provide MEMORY threshold as arguement.
+	exit 0
 fi
-fi
-done
-sleep 1
+
+MAX_MEMORY=$1
+
+processes=$(ps -o pid ax | grep -v PID)
+
+for p in $processes; do
+	memory=$(pmap $p | tail -n 1 | awk -F '[^0-9]*' '/[0-9]K/{ print $2 }')
+	if [ ! -z $memory ] && [ $memory -gt $MAX_MEMORY ]; then
+		echo Killing Process\(PID:$p\) with Memory:${memory}K
+		# kill $p
+	fi
 done
